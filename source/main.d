9ddadd76e9431e494403f9e79d6bddf8d014efb5
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-01-25 09:37:59 -0800

    Added check for signal method matching class method

--- source/main.d
+++ source/main.d
@@ -12,26 +12,39 @@ import verify_d_code;
 void printErrors(Project project, KlassInfo[] class_infos) {
 	import std.stdio : stderr;
 	import std.string : format;
+	import std.algorithm : canFind;
 
 	// Print out any errors
 	foreach (Scene scene ; project._scenes.values()) {
 		string[] errors;
-		foreach (RefExtResource resource ; scene._resources) {
-			switch (resource._type) {
-				case "PackedScene":
-					Scene child_scene = project._scenes[resource._path];
-					if (child_scene._error) {
-						errors ~= "    error: %s".format(child_scene._error);
+
+		if (scene._error) {
+			errors ~= "    error: %s".format(scene._error);
+		} else {
+			// Get the class name from .tscn -> .gdns -> class_name
+			string class_name = null;
+			foreach (RefExtResource resource ; scene._resources) {
+				if (resource._type == "Script") {
+					NativeScript script = project._scripts[resource._path];
+					class_name = script._class_name;
+				}
+			}
+
+			// Get the signal method names
+			string[] methods;
+			foreach (RefConnection connection ; scene._connections) {
+				methods ~= connection._method;
+			}
+
+			// Make sure the classes have the methods
+			foreach (class_info ; class_infos) {
+				if (class_name == "%s.%s".format(class_info._module, class_info.class_name)) {
+					foreach (method ; methods) {
+						if (! class_info.methods.canFind(method)) {
+							errors ~= `    Signal method "%s" not found in class "%s"`.format(method, class_name);
+						}
 					}
-					break;
-				case "Script":
-					NativeScript child_script = project._scripts[resource._path];
-					if (child_script._error) {
-						errors ~= "    error: %s".format(child_script._error);
-					}
-					break;
-				default:
-					break;
+				}
 			}
 		}
 
@@ -45,10 +58,9 @@ void printErrors(Project project, KlassInfo[] class_infos) {
 
 	foreach (NativeScript script ; project._scripts.values()) {
 		string[] errors;
-		NativeLibrary child_library = project._libraries[script._native_library._path];
 
-		if (child_library._error) {
-			errors ~= "    error: %s".format(child_library._error);
+		if (script._error) {
+			errors ~= "    error: %s".format(script._error);
 		}
 
 		if (errors.length > 0) {
@@ -81,16 +93,7 @@ int main() {
 	//
 	auto class_infos = getCodeClasses(project_path ~ `src\`);
 
-	//printInfo(project);
-	//printErrors(project);
 	printErrors(project, class_infos);
-/*
-	foreach (info ; class_infos) {
-		stdout.writefln("%s.%s : %s", info._module, info.class_name, info.base_class_name); stdout.flush();
-		foreach (method ; info.methods) {
-			stdout.writefln("    method: %s", method); stdout.flush();
-		}
-	}
-*/
+
 	return 0;
 }
