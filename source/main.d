6d019332a1494254128ffb4060a2d1381da588e6
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-01-25 07:20:11 -0800

    Moved error printing inside main file

--- source/main.d
+++ source/main.d
@@ -4,7 +4,72 @@
 // https://github.com/workhorsy/godot-d-verify
 
 
+import std.stdio : stdout;
+import verify_godot;
+import verify_d_code;
 
+
+void printErrors(Project project, KlassInfo[] class_infos) {
+	import std.stdio : stderr;
+	import std.string : format;
+
+	// Print out any errors
+	foreach (Scene scene ; project._scenes.values()) {
+		string[] errors;
+		foreach (RefExtResource resource ; scene._resources) {
+			switch (resource._type) {
+				case "PackedScene":
+					Scene child_scene = project._scenes[resource._path];
+					if (child_scene._error) {
+						errors ~= "    error: %s".format(child_scene._error);
+					}
+					break;
+				case "Script":
+					NativeScript child_script = project._scripts[resource._path];
+					if (child_script._error) {
+						errors ~= "    error: %s".format(child_script._error);
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		if (errors.length > 0) {
+			stderr.writefln("tscn: %s", scene._path); stderr.flush();
+			foreach (error ; errors) {
+				stderr.writefln("%s", error); stderr.flush();
+			}
+		}
+	}
+
+	foreach (NativeScript script ; project._scripts.values()) {
+		string[] errors;
+		NativeLibrary child_library = project._libraries[script._native_library._path];
+
+		if (child_library._error) {
+			errors ~= "    error: %s".format(child_library._error);
+		}
+
+		if (errors.length > 0) {
+			stderr.writefln("gdns: %s", script._path); stderr.flush();
+			foreach (error ; errors) {
+				stderr.writefln("%s", error); stderr.flush();
+			}
+		}
+	}
+
+	foreach (NativeLibrary library ; project._libraries.values()) {
+		string[] errors;
+
+		if (errors.length > 0) {
+			stderr.writefln("gdnlib: %s", library._path); stderr.flush();
+			foreach (error ; errors) {
+				stderr.writefln("%s", error); stderr.flush();
+			}
+		}
+	}
+}
 
 int main() {
 	stdout.writefln("Verifying godot project ..."); stdout.flush();
