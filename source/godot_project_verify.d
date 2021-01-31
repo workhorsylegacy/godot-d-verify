92a14bcb45fb34c077f3ae068329ab2a738ad840
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-01-31 13:17:38 -0800

    Made verify visitors private

--- source/godot_project_verify.d
+++ source/godot_project_verify.d
@@ -11,6 +11,165 @@ import godot_project_parse;
 
 
 
+string[][string] verifyProject(string project_path, Project project, KlassInfo[] class_infos) {
+	import std.string : format;
+	import std.algorithm : filter;
+	import std.array : assocArray, byPair;
+
+	string[][string] retval;
+
+	// Check projects
+	foreach (Project proj ; [project]) {
+		if (proj._error) continue;
+		string[] errors;
+		errors ~= new MainSceneVerifyProjectVisitor().visit(project_path, proj, class_infos);
+		if (errors.length) retval[proj._path] = errors;
+	}
+
+	// Check scenes
+	foreach (scene ; project._scenes.values.sortBy!(Scene, "_path")) {
+		if (scene._error) continue;
+		string[] errors;
+		errors ~= new ResourceVerifySceneVisitor().visit(scene, project_path, project, class_infos);
+		errors ~= new SignalMethodInCodeVerifySceneVisitor().visit(scene, project_path, project, class_infos);
+		if (errors.length) retval["tscn: %s".format(scene._path)] = errors;
+	}
+
+	// Check scripts
+	foreach (script ; project._scripts.values.sortBy!(NativeScript, "_path")) {
+		if (script._error) continue;
+		string[] errors;
+		errors ~= new NativeLibraryVerifyScriptVisitor().visit(script, project_path, project, class_infos);
+		errors ~= new ClassNameVerifyScriptVisitor().visit(script, project_path, project, class_infos);
+		errors ~= new ScriptClassInCodeVerifyScriptVisitor().visit(script, project_path, project, class_infos);
+		if (errors.length) retval["gdns: %s".format(script._path)] = errors;
+	}
+
+	// Check libraries
+	foreach (library ; project._libraries.values.sortBy!(NativeLibrary, "_path")) {
+		if (library._error) continue;
+		string[] errors;
+		errors ~= new SymbolPrefixVerifyLibraryVisitor().visit(library, project_path, project, class_infos);
+		errors ~= new DllPathVerifyLibraryVisitor().visit(library, project_path, project, class_infos);
+		if (errors.length) retval["gdnlib: %s".format(library._path)] = errors;
+	}
+
+	// Remove any empty error arrays
+	retval = retval
+			.byPair
+			.filter!(pair => pair.value.length > 0)
+			.assocArray;
+
+	return retval;
+}
+
+
+unittest {
+	import BDD;
+
+	import godot_project_parse : parseProject, printInfo;
+	import scan_d_code : getCodeClasses;
+	import std.file : getcwd, chdir;
+
+	string[][string] setupTest(string project_path) {
+		project_path = absolutePath(project_path);
+		auto project = parseProject(project_path ~ `project/project.godot`);
+		auto class_infos = getCodeClasses(project_path ~ `src/`);
+		return verifyProject(project_path ~ `project/`, project, class_infos);
+	}
+
+	describe("godot_project_verify#project",
+		it("Should succeed on working project", () {
+			auto errors = setupTest(`test/project_normal/`);
+			errors.shouldEqual((string[][string]).init);
+		}),
+		it("Should fail when project main scene is not specified", () {
+			auto errors = setupTest(`test/project_main_scene_no_entry/`);
+			errors.shouldEqual([`project.godot`:
+				[`Project missing main scene`]
+			]);
+		}),
+		it("Should fail when project main scene file is not found", () {
+			auto errors = setupTest(`test/project_main_scene_no_file/`);
+			errors.shouldEqual([`project.godot`:
+				[`Project main scene file not found: "Level/XXX.tscn"`]
+			]);
+		})
+	);
+
+	describe("godot_project_verify#scene",
+		it("Should fail when scene resource file is not found", () {
+			auto errors = setupTest(`test/project_scene_resource_missing/`);
+			errors.shouldEqual([`tscn: Level/Level.tscn`:
+				[`Scene resource file not found: "Player/XXX.tscn"`]
+			]);
+		}),
+		it("Should fail when signal method doesn't exists in code", () {
+			auto errors = setupTest(`test/project_scene_signal_no_code_method/`);
+			errors.shouldEqual([`tscn: Level/Level.tscn`:
+				[`Signal method "xxx" not found in class "level.Level"`]
+			]);
+		}),
+		it("Should fail when signal method exists but missing Method attribute", () {
+			auto errors = setupTest(`test/project_scene_signal_no_method_attribute/`);
+			errors.shouldEqual([`tscn: Level/Level.tscn`:
+				[`Signal method "on_button_pressed" found in class "level.Level" but missing @Method attribute`]
+			]);
+		})
+	);
+
+	describe("godot_project_verify#script",
+		it("Should fail when script native library is not specified", () {
+			auto errors = setupTest(`test/project_script_resource_no_entry/`);
+			errors.shouldEqual([`gdns: Player/Player.gdns`:
+				[`Script missing native library`]
+			]);
+		}),
+		it("Should fail when script native library file is not found", () {
+			auto errors = setupTest(`test/project_script_resource_no_file/`);
+			errors.shouldEqual([`gdns: Player/Player.gdns`:
+				[`Script resource file not found: "XXX.gdnlib"`]
+			]);
+		}),
+		it("Should fail when script class_name is not specified", () {
+			auto errors = setupTest(`test/project_script_no_class_name/`);
+			errors.shouldEqual([`gdns: Player/Player.gdns`:
+				[`Script missing class_name`]
+			]);
+		}),
+		it("Should fail when script class does not exist in code", () {
+			auto errors = setupTest(`test/project_script_no_code_class/`);
+			errors.shouldEqual([`gdns: Player/Player.gdns`:
+				[`Script missing class "player.Player"`]
+			]);
+		})
+	);
+
+	describe("godot_project_verify#library",
+		it("Should fail when native library symbol_prefix is not specified", () {
+			auto errors = setupTest(`test/project_library_no_symbol_prefix_entry/`);
+			errors.shouldEqual([`gdnlib: libsimple.gdnlib`:
+				[`Library missing symbol_prefix`]
+			]);
+		}),
+		it("Should fail when native library dll/so file is not specified", () {
+			auto errors = setupTest(`test/project_library_no_dll_entry/`);
+
+			version (Windows) {
+				errors.shouldEqual([`gdnlib: libsimple.gdnlib`:
+					[`Library missing Windows.64`]
+				]);
+			} else version (linux) {
+				errors.shouldEqual([`gdnlib: libsimple.gdnlib`:
+					[`Library missing X11.64`]
+				]);
+			}
+		})
+	);
+}
+
+private:
+
 T[] sortBy(T, string field_name)(T[] things) {
 	import std.algorithm : sort;
 	import std.array : array;
@@ -217,161 +376,4 @@ class DllPathVerifyLibraryVisitor : VerifyLibraryVisitor {
 
 		return errors;
 	}
-}
-
-string[][string] verifyProject(string project_path, Project project, KlassInfo[] class_infos) {
-	import std.string : format;
-	import std.algorithm : filter;
-	import std.array : assocArray, byPair;
-
-	string[][string] retval;
-
-	// Check projects
-	foreach (Project proj ; [project]) {
-		if (proj._error) continue;
-		string[] errors;
-		errors ~= new MainSceneVerifyProjectVisitor().visit(project_path, proj, class_infos);
-		if (errors.length) retval[proj._path] = errors;
-	}
-
-	// Check scenes
-	foreach (scene ; project._scenes.values.sortBy!(Scene, "_path")) {
-		if (scene._error) continue;
-		string[] errors;
-		errors ~= new ResourceVerifySceneVisitor().visit(scene, project_path, project, class_infos);
-		errors ~= new SignalMethodInCodeVerifySceneVisitor().visit(scene, project_path, project, class_infos);
-		if (errors.length) retval["tscn: %s".format(scene._path)] = errors;
-	}
-
-	// Check scripts
-	foreach (script ; project._scripts.values.sortBy!(NativeScript, "_path")) {
-		if (script._error) continue;
-		string[] errors;
-		errors ~= new NativeLibraryVerifyScriptVisitor().visit(script, project_path, project, class_infos);
-		errors ~= new ClassNameVerifyScriptVisitor().visit(script, project_path, project, class_infos);
-		errors ~= new ScriptClassInCodeVerifyScriptVisitor().visit(script, project_path, project, class_infos);
-		if (errors.length) retval["gdns: %s".format(script._path)] = errors;
-	}
-
-	// Check libraries
-	foreach (library ; project._libraries.values.sortBy!(NativeLibrary, "_path")) {
-		if (library._error) continue;
-		string[] errors;
-		errors ~= new SymbolPrefixVerifyLibraryVisitor().visit(library, project_path, project, class_infos);
-		errors ~= new DllPathVerifyLibraryVisitor().visit(library, project_path, project, class_infos);
-		if (errors.length) retval["gdnlib: %s".format(library._path)] = errors;
-	}
-
-	// Remove any empty error arrays
-	retval = retval
-			.byPair
-			.filter!(pair => pair.value.length > 0)
-			.assocArray;
-
-	return retval;
-}
-
-
-unittest {
-	import BDD;
-
-	import godot_project_parse : parseProject, printInfo;
-	import scan_d_code : getCodeClasses;
-	import std.file : getcwd, chdir;
-
-	string[][string] setupTest(string project_path) {
-		project_path = absolutePath(project_path);
-		auto project = parseProject(project_path ~ `project/project.godot`);
-		auto class_infos = getCodeClasses(project_path ~ `src/`);
-		return verifyProject(project_path ~ `project/`, project, class_infos);
-	}
-
-	describe("godot_project_verify#project",
-		it("Should succeed on working project", () {
-			auto errors = setupTest(`test/project_normal/`);
-			errors.shouldEqual((string[][string]).init);
-		}),
-		it("Should fail when project main scene is not specified", () {
-			auto errors = setupTest(`test/project_main_scene_no_entry/`);
-			errors.shouldEqual([`project.godot`:
-				[`Project missing main scene`]
-			]);
-		}),
-		it("Should fail when project main scene file is not found", () {
-			auto errors = setupTest(`test/project_main_scene_no_file/`);
-			errors.shouldEqual([`project.godot`:
-				[`Project main scene file not found: "Level/XXX.tscn"`]
-			]);
-		})
-	);
-
-	describe("godot_project_verify#scene",
-		it("Should fail when scene resource file is not found", () {
-			auto errors = setupTest(`test/project_scene_resource_missing/`);
-			errors.shouldEqual([`tscn: Level/Level.tscn`:
-				[`Scene resource file not found: "Player/XXX.tscn"`]
-			]);
-		}),
-		it("Should fail when signal method doesn't exists in code", () {
-			auto errors = setupTest(`test/project_scene_signal_no_code_method/`);
-			errors.shouldEqual([`tscn: Level/Level.tscn`:
-				[`Signal method "xxx" not found in class "level.Level"`]
-			]);
-		}),
-		it("Should fail when signal method exists but missing Method attribute", () {
-			auto errors = setupTest(`test/project_scene_signal_no_method_attribute/`);
-			errors.shouldEqual([`tscn: Level/Level.tscn`:
-				[`Signal method "on_button_pressed" found in class "level.Level" but missing @Method attribute`]
-			]);
-		})
-	);
-
-	describe("godot_project_verify#script",
-		it("Should fail when script native library is not specified", () {
-			auto errors = setupTest(`test/project_script_resource_no_entry/`);
-			errors.shouldEqual([`gdns: Player/Player.gdns`:
-				[`Script missing native library`]
-			]);
-		}),
-		it("Should fail when script native library file is not found", () {
-			auto errors = setupTest(`test/project_script_resource_no_file/`);
-			errors.shouldEqual([`gdns: Player/Player.gdns`:
-				[`Script resource file not found: "XXX.gdnlib"`]
-			]);
-		}),
-		it("Should fail when script class_name is not specified", () {
-			auto errors = setupTest(`test/project_script_no_class_name/`);
-			errors.shouldEqual([`gdns: Player/Player.gdns`:
-				[`Script missing class_name`]
-			]);
-		}),
-		it("Should fail when script class does not exist in code", () {
-			auto errors = setupTest(`test/project_script_no_code_class/`);
-			errors.shouldEqual([`gdns: Player/Player.gdns`:
-				[`Script missing class "player.Player"`]
-			]);
-		})
-	);
-
-	describe("godot_project_verify#library",
-		it("Should fail when native library symbol_prefix is not specified", () {
-			auto errors = setupTest(`test/project_library_no_symbol_prefix_entry/`);
-			errors.shouldEqual([`gdnlib: libsimple.gdnlib`:
-				[`Library missing symbol_prefix`]
-			]);
-		}),
-		it("Should fail when native library dll/so file is not specified", () {
-			auto errors = setupTest(`test/project_library_no_dll_entry/`);
-
-			version (Windows) {
-				errors.shouldEqual([`gdnlib: libsimple.gdnlib`:
-					[`Library missing Windows.64`]
-				]);
-			} else version (linux) {
-				errors.shouldEqual([`gdnlib: libsimple.gdnlib`:
-					[`Library missing X11.64`]
-				]);
-			}
-		})
-	);
 }
