4be6a8f22f3f0a6a77effc6c0397654fbb6df997
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-02-01 15:04:29 -0800

    Added missing imports

--- source/main.d
+++ source/main.d
@@ -9,6 +9,7 @@ int main(string[] args) {
 	import std.stdio : stdout, stderr;
 	import std.algorithm : canFind, endsWith;
 	import std.file : chdir;
+	import helpers : dirName, buildPath;
 	import godot_project_parse : parseProject;
 	import scan_d_code : getCodeClasses;
 	import godot_project_verify : verifyProject;
